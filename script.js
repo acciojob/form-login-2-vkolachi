be0dbc30d697fbadde1fd6c4f2f6656bd380673b
vkolachi <36785379+vkolachi@users.noreply.github.com>
2023-12-03 13:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function sub() {
+	var fnme=document.getElementById("fname").value;
+	var lnme=document.getElementById("lname").value;
+	var phn=document.getElementById("phone").value;
+	var eml=document.getElementById("email").value;
+
+	var alertmsg="First Name:"+fnme+ "\nLast Name:"+lnme+"\nPhone Number:"+phn+"\nEmail ID:"+eml;
+	alert(alertmsg);
+	
+}
